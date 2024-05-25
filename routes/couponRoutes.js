b2cfa6b711079c7a2a86a95d22504d199dc833ec
Ashufr <harashish03@gmail.com>
2024-05-25 16:21:18 +0530

    feat: impliment Coupon routes
    Add routes for cart CRUD operations with middleware protection to ensure only authorized users can access these routes.

---
+++ routes/couponRoutes.js
@@ -0,0 +1,26 @@
+import express from "express";
+import {
+    getAllCoupons,
+    getCoupon,
+    getCouponByCode,
+    createCoupon,
+    createMultipleCoupons,
+    updateCoupon,
+    deleteCoupon,
+    validateCouponAndReturnDiscount,
+  } from "../controllers/couponController.js";
+
+import { protect, adminProtect } from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+router.get("/", getAllCoupons);
+router.get("/:id", getCoupon);
+router.get("/code/:code", getCouponByCode);
+router.get("/validate/:code", protect, validateCouponAndReturnDiscount);
+router.post("/", adminProtect, createCoupon);
+router.post("/multiple", adminProtect, createMultipleCoupons);
+router.put("/:id", adminProtect, updateCoupon);
+router.delete("/:id", adminProtect, deleteCoupon);
+
+export default router;
