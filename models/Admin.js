d1cecc8e40f88ea2edf2e5a533016972a5a6bc5b
Ashufr <harashish03@gmail.com>
2024-05-25 12:28:27 +0530

    fix: resolve function issue for both admin and user
    Fix a bug in the function related to both admin and user functionalities to ensure correct behavior.

--- models/Admin.js
+++ models/Admin.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
 
 const adminSchema = new mongoose.Schema({
   email: {
@@ -12,14 +13,14 @@ const adminSchema = new mongoose.Schema({
   },
 });
 
-adminSchema.pre("save", async (next) => {
+adminSchema.pre("save", async function(next) {
   if (!this.isModified("password")) return next();
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
   next();
 });
 
-adminSchema.methods.matchPassword = async (enteredPassword) => {
+adminSchema.methods.matchPassword = async function(enteredPassword) {
   return await bcrypt.compare(enteredPassword, this.password);
 };
 
