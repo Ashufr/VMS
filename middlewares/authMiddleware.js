fda94ef218b08e2561ab36324011f0691d4506a9
Ashufr <harashish03@gmail.com>
2024-05-25 11:47:20 +0530

    feat: add authentication middleware
    Create authMiddleware.js to implement authentication middleware for protecting routes and verifying user authentication tokens.

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,46 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+import Admin from "../models/Admin.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const protect = async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = await User.findById(decoded.id).select("-password");
+      next();
+    } catch (error) {
+      res.status(401).json({ message: "Not authorized, token failed" });
+    }
+  } else {
+    res.status(401).json({ message: "Not authorized, token not found" });
+  }
+};
+
+const adminProtect = async (req, res, next) => {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    try {
+      token = req.headers.authorization.split(" ")[1];
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.admin = await Admin.findById(decoded.id).select("-password");
+      next();
+    } catch (error) {
+      res.status(401).json({ message: "Not authorized, token failed" });
+    }
+  } else {
+    res.status(401).json({ message: "Not authorized, no token" });
+  }
+};
+
+export { protect, adminProtect };
