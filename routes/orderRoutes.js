15a500f8533b452bf29da7aadb7fd87b12aa44a1
Ashufr <harashish03@gmail.com>
2024-05-25 18:02:18 +0530

    feat: impliment Order routes
    Add routes for order CRUD operations with middleware protection to ensure only authorized users can access these routes.

---
+++ routes/orderRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import { createOrder, getOrderDetails, getAllOrders, getAllUserOrders } from "../controllers/orderController.js";
+
+import { protect, adminProtect } from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+router.post("/", protect, createOrder);
+router.get("/:id",protect, getOrderDetails);
+router.get("/", adminProtect, getAllOrders);
+router.get("/user-orders", protect, getAllUserOrders);
